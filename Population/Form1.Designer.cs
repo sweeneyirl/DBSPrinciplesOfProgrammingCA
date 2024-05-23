55f4ecbbe615a373b198efc9d43c44071e7efcfd
sweeneyirl <107083551+sweeneyirl@users.noreply.github.com>
2024-05-23 18:45:23 +0100

    pop

---
+++ Population/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Population
+{
+    partial class Population
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.orgsLabel = new System.Windows.Forms.Label();
+            this.incLabel = new System.Windows.Forms.Label();
+            this.daysLabel = new System.Windows.Forms.Label();
+            this.outputListbox = new System.Windows.Forms.ListBox();
+            this.orgsTextbox = new System.Windows.Forms.TextBox();
+            this.incTextbox = new System.Windows.Forms.TextBox();
+            this.daysTextbox = new System.Windows.Forms.TextBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // orgsLabel
+            // 
+            this.orgsLabel.AutoSize = true;
+            this.orgsLabel.Location = new System.Drawing.Point(12, 19);
+            this.orgsLabel.Name = "orgsLabel";
+            this.orgsLabel.Size = new System.Drawing.Size(185, 16);
+            this.orgsLabel.TabIndex = 0;
+            this.orgsLabel.Text = "Starting Number of Organisms";
+            // 
+            // incLabel
+            // 
+            this.incLabel.AutoSize = true;
+            this.incLabel.Location = new System.Drawing.Point(12, 68);
+            this.incLabel.Name = "incLabel";
+            this.incLabel.Size = new System.Drawing.Size(148, 16);
+            this.incLabel.TabIndex = 1;
+            this.incLabel.Text = "Average Daily Increase";
+            // 
+            // daysLabel
+            // 
+            this.daysLabel.AutoSize = true;
+            this.daysLabel.Location = new System.Drawing.Point(12, 117);
+            this.daysLabel.Name = "daysLabel";
+            this.daysLabel.Size = new System.Drawing.Size(104, 16);
+            this.daysLabel.TabIndex = 2;
+            this.daysLabel.Text = "Number of Days";
+            // 
+            // outputListbox
+            // 
+            this.outputListbox.ColumnWidth = 85;
+            this.outputListbox.FormattingEnabled = true;
+            this.outputListbox.ItemHeight = 16;
+            this.outputListbox.Location = new System.Drawing.Point(246, 19);
+            this.outputListbox.Name = "outputListbox";
+            this.outputListbox.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.outputListbox.Size = new System.Drawing.Size(478, 212);
+            this.outputListbox.TabIndex = 3;
+            // 
+            // orgsTextbox
+            // 
+            this.orgsTextbox.Location = new System.Drawing.Point(12, 38);
+            this.orgsTextbox.Name = "orgsTextbox";
+            this.orgsTextbox.Size = new System.Drawing.Size(100, 22);
+            this.orgsTextbox.TabIndex = 4;
+            this.orgsTextbox.TextChanged += new System.EventHandler(this.orgsTextbox_TextChanged);
+            // 
+            // incTextbox
+            // 
+            this.incTextbox.Location = new System.Drawing.Point(15, 87);
+            this.incTextbox.Name = "incTextbox";
+            this.incTextbox.Size = new System.Drawing.Size(100, 22);
+            this.incTextbox.TabIndex = 5;
+            this.incTextbox.TextChanged += new System.EventHandler(this.incTextbox_TextChanged);
+            // 
+            // daysTextbox
+            // 
+            this.daysTextbox.Location = new System.Drawing.Point(15, 136);
+            this.daysTextbox.Name = "daysTextbox";
+            this.daysTextbox.Size = new System.Drawing.Size(100, 22);
+            this.daysTextbox.TabIndex = 6;
+            this.daysTextbox.TextChanged += new System.EventHandler(this.daysTextbox_TextChanged);
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(15, 164);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 7;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // Population
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.daysTextbox);
+            this.Controls.Add(this.incTextbox);
+            this.Controls.Add(this.orgsTextbox);
+            this.Controls.Add(this.outputListbox);
+            this.Controls.Add(this.daysLabel);
+            this.Controls.Add(this.incLabel);
+            this.Controls.Add(this.orgsLabel);
+            this.Name = "Population";
+            this.Text = "Population";
+            this.Load += new System.EventHandler(this.Population_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label orgsLabel;
+        private System.Windows.Forms.Label incLabel;
+        private System.Windows.Forms.Label daysLabel;
+        private System.Windows.Forms.ListBox outputListbox;
+        private System.Windows.Forms.TextBox orgsTextbox;
+        private System.Windows.Forms.TextBox incTextbox;
+        private System.Windows.Forms.TextBox daysTextbox;
+        private System.Windows.Forms.Button runButton;
+    }
+}
+
---
+++ Population/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Population
+{
+    public partial class Population : Form
+    {
+        public Population()
+        {
+            InitializeComponent();
+        }
+        private void Population_Load(object sender, EventArgs e)
+        {
+
+        }
+        private class LBOutput
+        {
+            public LBOutput(int orgs, int increase, int days)
+            {
+                this.Orgs = orgs;
+                this.Increase = increase;
+                this.Days = days;
+            }
+            public int Orgs { get; set; }
+            public int Increase { get; set; }
+            public int Days { get; set; }
+
+        }
+        private void runButton_Click(object sender, EventArgs e)
+        {
+        
+            if (int.TryParse(daysTextbox.Text, out int days))
+            {
+                if (int.TryParse(orgsTextbox.Text, out int orgs))
+                {
+                    if (int.TryParse(incTextbox.Text, out int inc))
+                    {
+                        
+                        
+                        outputListbox.Items.Add("Yiss");
+
+                    }
+                    else { outputListbox.Items.Add("Increase Textbox invalid input. Use Numbers"); }
+                } else { outputListbox.Items.Add("Org Textbox invalid input. Use numbers."); }
+            } else { outputListbox.Items.Add("Days Textbox invalid input. Use Numbers"); }
+        }
+
+        private void orgsTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void incTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void daysTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
